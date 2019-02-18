9eedfe405ba6a43caa0315e505ffce448f83a496
LeidenJar <hx.luan128@gmail.com>
2019-02-18 19:35:03 -0300

    JustToPush

--- application.py
+++ application.py
@@ -1,16 +1,16 @@
 import os
 
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_session import Session
-import requests
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-app.config["SESSION_PERMANENT"] = 'False'
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 now = datetime.datetime.now()
@@ -29,7 +29,5 @@ def index():
     return render_template("home.html")
 @app.route("/channels", methods=['POST'])
 def canais():
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'}
-
-	user = request.form.get('nome', headers=readers)
-	return user
+	user = request.form.get('nome')
+	return render_template('channels.html', x=user)
