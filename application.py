10d10d711ee233dc0099b4771bfa5bbc6f1b3a7a
LeidenJar <hx.luan128@gmail.com>
2019-02-22 14:54:48 -0300

    Users are remembered via session and can make logout

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
 import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_session import Session
 
@@ -11,6 +11,8 @@ socketio = SocketIO(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 now = datetime.datetime.now()
 
@@ -25,16 +27,37 @@ def getlog():
 
 @app.route("/")
 def index():
-    return render_template("home.html")
-@app.route("/channels", methods=['POST'])
+	u = getlog()
+	if u == None:
+		return render_template('home.html')
+	else:
+		return redirect('/channels')
+
+
+
+@app.route("/channels", methods=['POST', 'GET'])
 def canais():
-	user = request.form.get('user')
-	return render_template('channels.html', x=user)
+
+	if request.method=='GET':
+		u = getlog()
+		if u == None:
+			return redirect('/')
+		else:
+			return render_template('channels.html', x=session['user'])
+	else:
+		session['user'] = request.form.get('user')
+		return render_template('channels.html', x=session['user'])
+
+@app.route("/logout")
+def logout():
+	session['user']=None
+	return redirect('/')
 
 @socketio.on('send_message')
 def message(msg):
 	mensagem = msg["mensagem"]
-	emit('broadcast_message', {'mensagem' : mensagem}, broadcast=True)
+	user = msg["user"]
+	emit('broadcast_message', {'mensagem' : mensagem, 'user' : user}, broadcast=True)
 
 if __name__ == "__main__":
 	socketio.run(app, debug=True)
